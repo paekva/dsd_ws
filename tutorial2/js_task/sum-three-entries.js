50f80bd20ce6290dddfe897349f93c974f386411
paekva <ekaterina.pavlova@siemens.com>
2021-11-20 12:10:43 +0100

    tutorial 2: setup and password task

---
+++ tutorial2/js_task/sum-three-entries.js
@@ -0,0 +1,10 @@
+/** 
+ * Credit to https://adventofcode.com/ for this exercise
+ * The list below provides a set of numbers. The objective is to find the three numbers that sum up to 2021. 
+ * After finding those numbers, multiply them and store the output in the results variable.
+*/ 
+
+const numbers = [1722, 920, 299, 446, 1011, 655];
+let results;
+
+// Write your code here
