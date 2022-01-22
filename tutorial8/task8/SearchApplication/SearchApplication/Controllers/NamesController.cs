c57e1540bf50d90cef68d9a54c2e8ebe17b62435
paekva <paekva@yandex.ru>
2022-01-22 23:13:55 +0300

    task 8 setup + simple page

---
+++ tutorial8/task8/SearchApplication/SearchApplication/Controllers/NamesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SearchApplication.Models;
+
+namespace SearchApplication.Controllers
+{
+    public class NamesController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Search(string search)
+        {
+            var namesModel = new Names("names.txt");
+            namesModel.PerformSearch(search);
+
+            return View(namesModel);
+        }
+    }
+}
---
+++ tutorial8/task8/SearchApplication/SearchApplication/Models/Names.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace SearchApplication.Models
+{
+    public class Names
+    {
+        public List<string> NameList { get; set; }
+        public List<string> SearchedNameList { get; set; }
+        public string SearchValue { get; set; }
+
+        public Names(string filePath) {
+            try
+            {
+                if (!File.Exists(filePath)) {
+                    NameList = new List<string>();
+                    return;
+                }
+
+                string[] readText = File.ReadAllLines(filePath);
+                foreach (string s in readText)
+                {
+                    NameList.Add(s);
+                }
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void PerformSearch(string search)
+        {
+            SearchedNameList = NameList.Where(name => name.Contains(search)).ToList();
+            SearchValue = search;
+        }
+    }
+}
